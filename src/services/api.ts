05037074ddc78273ac2a3d1a94b71019b366199a
Louise Rosenqvist <93682181+lrosenqv@users.noreply.github.com>
2022-10-27 13:29:24 +0200

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import { ILogin } from "../models/ILogin";
 import { INewUser } from "../models/INewUser";
 import { IUser } from "../models/IUser";
 
-const url: string = "http://localhost:3001/users"
+const url: string = "https://dw-newsletter.herokuapp.com/users"
 export class ApiService {
   async getUser(id: string){
     let response = await axios.get<IUser>(`${url}/${id}`)
@@ -25,7 +25,7 @@ export class ApiService {
   }
 
   async login(user: ILogin){
-    let response = await axios.post('http://localhost:3001/login', user)
+    let response = await axios.post('https://dw-newsletter.herokuapp.com/login', user)
     .then(res => {
       if(res.data !== "Not found"){
         localStorage.setItem('onlineUserKey', JSON.stringify(res.data._id))
@@ -39,7 +39,7 @@ export class ApiService {
   }
 
   addUser(newUser: INewUser){
-    axios.post('http://localhost:3001/addUser', newUser)
+    axios.post('https://dw-newsletter.herokuapp.com/addUser', newUser)
     .then(res => {
       console.log(res.data);
     })
